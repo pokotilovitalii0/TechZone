5cb225ffdc3301726cbb29fe579951971e659919
VitaliiPokotilo <pokotilovitalii0@gmail.com>
2025-12-26 09:31:58 +0200

    feat: add scroll to top when open new page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import Layout from './components/layout/Layout';
 import HomePage from './pages/home/HomePage';
 import CatalogPage from './pages/catalog/CatalogPage';
 import CartPage from './pages/cart/CartPage';
-import ProductPage from './pages/catalog/ProductPage'; // Перевірте шлях імпорту, у вас було '../pages/catalog/ProductPage'
+import ProductPage from './pages/catalog/ProductPage';
 import RegisterPage from './pages/auth/RegisterPage';
 import LoginPage from './pages/auth/LoginPage';
 import CheckoutPage from './pages/checkout/CheckoutPage';
@@ -15,36 +15,40 @@ import ContactsPage from './pages/info/ContactsPage';
 import DeliveryPage from './pages/info/DeliveryPage';
 import WarrantyPage from './pages/info/WarrantyPage';
 import ReturnsPage from './pages/info/ReturnsPage';
+import ScrollToTop from './components/ScrollToTop'; // Імпортуємо наш новий компонент
 
 function App() {
   return (
-    <Routes>
-      {/* Батьківський маршрут - Layout */}
-      <Route path="/" element={<Layout />}>
-        {/* Вкладені маршрути */}
-        <Route index element={<HomePage />} />
-        <Route path="catalog" element={<CatalogPage />} />
-        <Route path="/catalog/:categorySlug" element={<CatalogPage />} />
-        <Route path="/wishlist" element={<WishlistPage />} />
-        <Route path="cart" element={<CartPage />} />
+    <>
+      {/* Компонент, що слідкує за зміною URL і крутить скрол вгору */}
+      <ScrollToTop />
 
-        {/* --- ОСЬ ТУТ БУЛА ПОМИЛКА --- */}
-        {/* Було :id, стало :slug, щоб збігалося з useParams() у ProductPage */}
-        <Route path="/product/:slug" element={<ProductPage />} />
+      <Routes>
+        {/* Батьківський маршрут - Layout */}
+        <Route path="/" element={<Layout />}>
+          {/* Вкладені маршрути */}
+          <Route index element={<HomePage />} />
+          <Route path="catalog" element={<CatalogPage />} />
+          <Route path="/catalog/:categorySlug" element={<CatalogPage />} />
+          <Route path="/wishlist" element={<WishlistPage />} />
+          <Route path="cart" element={<CartPage />} />
 
-        <Route path="/checkout" element={<CheckoutPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/contacts" element={<ContactsPage />} />
-        <Route path="/delivery" element={<DeliveryPage />} />
-        <Route path="/warranty" element={<WarrantyPage />} />
-        <Route path="/returns" element={<ReturnsPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Route>
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/auth" element={<RegisterPage />} />
-    </Routes>
+          <Route path="/product/:slug" element={<ProductPage />} />
+
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contacts" element={<ContactsPage />} />
+          <Route path="/delivery" element={<DeliveryPage />} />
+          <Route path="/warranty" element={<WarrantyPage />} />
+          <Route path="/returns" element={<ReturnsPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/auth" element={<RegisterPage />} />
+      </Routes>
+    </>
   );
 }
 
